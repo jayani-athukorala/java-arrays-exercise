a6d823097a1b2a0a9f691a1871171ec2a9c3a5ce
Jayani Athukorala <sandunkauwu@gmail.com>
2026-03-11 13:28:22 +0100

    Complete array-coolections exercise

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,56 +1,85 @@
 package se.lexicon;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     static void main(){
 
-        IO.println("Hello and welcome!");
-
+        IO.println("==========Hello and welcome!============");
         //Exercise 1:
+        IO.println("Exercise 1: Store elements in an array of type int and print it out.");
         IO.println(Arrays.toString(storePrintArray()));
+        IO.println("----------------------------------------\n");
 
         //Exercise 2:
+        IO.println("Exercise 2: Create a method 'indexOf' which will find and return the index of an array element");
         int[] indexArray = {12, 7, 5, 2, 8, 11};
         int returnedIndex = indexOf(indexArray);
         IO.println("Index position of "+indexArray[returnedIndex]+" is : "+returnedIndex);
+        IO.println("----------------------------------------\n");
 
-        //Exercise3
+        //Exercise 3:
+        IO.println("Exercise 3: Sort a string array");
         String[] strArray = {"Paris", "London", "New York", "Stockholm"};
         String sortedArray = sortStringArray(strArray);
         IO.println(Arrays.toString(strArray)+"\n"+sortedArray);
+        IO.println("----------------------------------------\n");
 
         //Exercise 4:
+        IO.println("Exercise 4: Copy the elements of one array into another array");
         int[] firstArray = {1, 15, 20, 35};
         IO.println(String.format("Elements from first array: %s \n" +
                         "Elements from second array: %s",
                 Arrays.toString(firstArray), Arrays.toString(copyElementsInArray(firstArray))));
+        IO.println("----------------------------------------\n");
 
         //Exercise 5:
+        IO.println("Exercise 5: Create a two-dimensional string array [2][2] and assign values");
         String[][] twoDArray = twoDimensionalArray();
         IO.println(Arrays.deepToString(twoDArray));
+        IO.println("----------------------------------------\n");
 
         //Exercise 6:
+        IO.println("Exercise 6: Print the average of 6 numbers holds in an array");
         int[] numbers = { 43, 5, 23, 17, 2, 14};
         double average = findAverage(numbers);
         IO.println("Average is : "+ average);
+        IO.println("----------------------------------------\n");
 
         //Exercise 7:
+        IO.println("Exercise 7: Set up an array to hold 10 numbers and print out only the uneven numbers");
         int[] numbersArray = {1, 2, 4,7, 9, 12};
         IO.println(String.format("Array : %s\nOdd Array : %s",
                 Arrays.toString(numbersArray), Arrays.toString(findOddNumbersArray(numbersArray))));
+        IO.println("----------------------------------------\n");
 
         //Exercise 8:
+        IO.println("Exercise 8: Remove the duplicate elements of a given array");
         int[] numbersArray2 ={20, 20, 40, 20, 30, 40, 50, 60, 50};
         IO.println(String.format("Array : %s\nDistinct Array : %s",
                 Arrays.toString(numbersArray2), Arrays.toString(removeDuplicates(numbersArray2))));
+        IO.println("----------------------------------------\n");
 
         //Exercise 9:
-
+        IO.println("Exercise 9: ArrayList -> “Expand” the array as user needs");
+        ArrayList<Integer> numbersList = addArrayElements();
+        IO.println("Expanded Array List: "+numbersList);
+        IO.println("----------------------------------------\n");
 
         //Exercise 10:
+        IO.println("Exercise 10: Ask the user to enter number or to exit, and when exit display the ArrayList in reverse order");
+        ArrayList<Integer> numbersArrayList = addArrayElements();
+        IO.println("Original Array: "+numbersList);
+        ArrayList<Integer> numbersArrayListReversed = reverseArray(numbersArrayList);
+        IO.println("Reversed Array: "+numbersArrayListReversed);
+        IO.println("----------------------------------------\n");
+
+        //Exercise 11:
+        IO.println("Exercise 11: Represent multiplication table using two‑dimensional array");
         int[] multipliers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[][] multiplicationTable = createMultiplicationTable(multipliers);
         for (int[] row : multiplicationTable) {
@@ -59,9 +88,14 @@ public class Main {
             }
             IO.println();
         }
+        IO.println("----------------------------------------\n");
 
-        //Exercise 11:
-        reverseArray();
+        //Exercise 12:
+        IO.println("Exercise 12: Copy random numbers from arraylist1 to arraylist2 in a way that,\n" +
+                " the even numbers are located in the rear (the right side) part of the array \n" +
+                "and the odd numbers are located in the front part (the left side)");
+        copyArrayLists();
+        IO.println("==============Bye Bye!===================");
     }
 
     // This method stores elements in an array of type int and print it out.
@@ -146,6 +180,23 @@ public class Main {
                 .toArray();
     }
 
+    //This method will let the user add elements to arraylist until they want to stop
+    static ArrayList<Integer> addArrayElements(){
+        ArrayList<Integer> numbersList = new ArrayList<>();
+        int newNumber;
+        do{
+
+            IO.print("Enter a number  (Enter -1 to exit): ");
+            newNumber = validateInt();
+            if(newNumber != -1) {
+                numbersList.add(newNumber);
+            }
+        } while(newNumber != -1);
+
+        return numbersList;
+    }
+
+
     //This method will return multiplication table, stored in multidimensional array
     static int[][] createMultiplicationTable(int[] multipliers){
         int[][] multiplicationTable = new int[10][10];
@@ -159,23 +210,10 @@ public class Main {
         return multiplicationTable;
     }
 
-    //This method will let the user decide array size,
-    // Then take user inputs until fills the array and then reverse the array elements and print it.
-    static void reverseArray(){
-
-        IO.print("How many numbers you want to Enter: ");
-        int arraySize = validateInt();
-
-        Integer[] newArray = new Integer[arraySize];
-
-        for(int i=0;i<newArray.length; i++){
-
-            IO.print("Enter ["+(i+1)+"] number of "+arraySize+" Numbers: ");
-            newArray[i] = validateInt();
-        }
-        IO.println("Original Array: "+Arrays.toString(newArray));
-        Collections.reverse(Arrays.asList(newArray));
-        IO.println("Reversed Array: "+Arrays.toString(newArray));
+    //This method reverse the array elements in an ArrayList and return it.
+    static ArrayList<Integer> reverseArray(ArrayList<Integer> arrayList){
+        Collections.reverse(arrayList);
+        return arrayList;
     }
 
     static int validateInt(){
@@ -190,4 +228,29 @@ public class Main {
             }
         }
     }
+
+    //
+    static void copyArrayLists(){
+        int size = 10; // arbitrary size
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        Random rand = new Random();
+
+        // Fill first list with random numbers
+        for (int i = 0; i < size; i++) {
+            int randomNumber = rand.nextInt(101);
+            list1.add(randomNumber); // 0–100
+            if(randomNumber%2 ==0){
+                list2.add(randomNumber);
+            }else{
+                list2.addFirst(randomNumber);
+            }
+        }
+
+        IO.println(list1);
+        IO.println(list2);
+    }
+
 }
